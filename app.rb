6842533a09ec78216d210893e73b2ab49e84e7f3
Lucas Tarasconi <lucas@tarasconi.fr>
2019-02-09 16:50:04 +0100

    Index and post view

--- app.rb
+++ app.rb
@@ -2,6 +2,21 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require_relative "database"
 
-get "/" do
+
+get '/' do
   @database = DB
+  erb :index
+end
+
+get '/posts/:title' do
+  @database = DB
+  @database.each do |posts|
+    if posts[:title] == params[:title] 
+      @post = posts
+      puts "success"
+      puts @post
+      break
+    end
+  end
+  erb :posts
 end
